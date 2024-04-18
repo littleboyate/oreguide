073daa782a3f1b1ea5f55de0043a6bff11b7da32
Ton <nataponz@gmail.com>
2024-04-18 12:15:53 +0700

    modified:   setuppingpong.sh

--- setuppingpong.sh
+++ setuppingpong.sh
@@ -8,15 +8,14 @@ sudo apt-get install bc -y
 sudo apt-get install jq -y
 
 
-sudo apt update 
+sudo apt update -y
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg 
-sudo apt-key add - 
- sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
- sudo apt update -y
- sudo apt install -y docker-ce 
- sudo systemctl start docker 
- sudo systemctl enable docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
+sudo apt update -y
+sudo apt install -y docker-ce 
+sudo systemctl start docker 
+sudo systemctl enable docker
 
 
  echo 'deb http://security.ubuntu.com/ubuntu jammy-security main' 
